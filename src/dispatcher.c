29d24e04214feb8d6141b86e1765360af724a06c
Ben Walker <bwalke05@gmail.com>
2019-03-12 15:43:05 -0400

    Determine the event of the process

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -1,7 +1,30 @@
 #include "dispatcher.h"
 #include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+
+static const int TIME = 0, EVENT = 1;
+static const char *CREATE = "C", *EXIT = "E", *REQUEST = "R", *INTERRUPT = "I", *TIMER = "T";
+
+void dispatchLine(char **line) {
+   if (strcmp(line[EVENT], CREATE) == 0) {
+
+   } else if (strcmp(line[EVENT], EXIT) == 0) {
+
+   } else if (strcmp(line[EVENT], REQUEST) == 0) {
+      
+   } else if (strcmp(line[EVENT], INTERRUPT) == 0) {
+      
+   } else if (strcmp(line[EVENT], TIMER) == 0) {
+      
+   }
+}
 
 void startDispatching() {
+   char **processLine;
 
+   while ((processLine = parseLine()) != NULL) {
+      dispatchLine(processLine);
+   }
 }
