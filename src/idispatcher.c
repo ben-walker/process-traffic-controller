33aa9f613b923749a28bbe46a3a32d4925306c7d
Ben Walker <bwalke05@gmail.com>
2019-03-13 10:55:14 -0400

    Missed one strcmp -> strncmp conversion

--- src/idispatcher.c
+++ src/idispatcher.c
@@ -9,7 +9,7 @@ static const int TIME_I = 0, EVENT_I = 1;
 static const char *CREATE = "C", *EXIT = "E", *REQUEST = "R", *INTERRUPT = "I", *TIMER = "T";
 
 void dispatch(char **event) {
-   if (strcmp(event[EVENT_I], CREATE) == 0) {
+   if (strncmp(event[EVENT_I], CREATE, 1) == 0) {
 
    } else if (strncmp(event[EVENT_I], EXIT, 1) == 0) {
 
