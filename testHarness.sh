6eca9a9a5904c87ca6c32a1f6a770fc27fb45d64
Ben Walker <bwalke05@gmail.com>
2019-03-18 10:09:14 -0400

    Add the final 3 test cases

--- testHarness.sh
+++ testHarness.sh
@@ -16,3 +16,18 @@ done
 ## Public testing is done!
 echo "Public testing is done!"
 
+pritest_num=(9 10 11)
+## Start Secret testing
+echo "Start secret testing ..."
+for tno in ${pritest_num[*]}
+do
+cp "pri-test-inputs/test$tno.in" "./events.txt"
+
+if cmp -s <(./idispatcher < "pri-test-inputs/test$tno.in") <(cat "pri-test-outputs/test$tno.out"); then
+   echo "Test $tno passed"
+else
+    echo "Test $tno failed"
+fi
+done
+## Secret testing is done!
+echo "Secret testing is done!"
